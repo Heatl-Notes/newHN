4d6b13e5a5c0cf9a288879d3499a1cf172095992
victorpazb <victor.braga@ccc.ufcg.edu.br>
2023-05-17 20:31:26 -0300

    first commit

---
+++ js/dashboard.js
@@ -0,0 +1,185 @@
+/**
+ * This file is responsible for the dashboard page
+ */
+
+//loadPatients function is called when the page is loaded to show the patients
+loadPatients();
+
+const buttons = document.querySelectorAll(".nav-btn");
+
+// Add event listener to each button
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // Remove "active" class from all buttons
+    buttons.forEach((button) => {
+      button.classList.remove("active");
+    });
+    // Add "active" class to clicked button
+    button.classList.add("active");
+  });
+});
+
+async function loadPatients() {
+  var main = document.querySelector(".main-content");
+  main.innerHTML = "";
+
+  //to up the server
+  //run the command:json-server --watch db.json (in this directory)
+
+  //REAL CODE
+  let patients = await fetch("http://localhost:3000/patients"); //fetch patients from api
+  let patientsJson = await patients.json();
+  let innerH = "";
+  let cpf = 0;
+  patientsJson.forEach((patient) => {
+    cpf = patient.cpf;
+    innerH = `<h4>Paciente:  ${patient.name} </h4> <br><span>Idade: ${patient.age} </span> <button onclick="deletePatient(${cpf})">excluir</button>`;
+    var novoElemento = document.createElement("div");
+    novoElemento.className = "patient-card";
+    novoElemento.innerHTML = innerH;
+
+    main.appendChild(novoElemento);
+  });
+}
+
+/**
+ *
+ */
+
+async function openProfile() {
+  let userId = 12345678919; //capturando o ID do usuario logado
+  let clients = await fetch("http://localhost:3000/clients"); //fetch patients from api
+  let clientsJson = await clients.json();
+  let courrentClient = null;
+  clientsJson.forEach((client) => {
+    if (client?.cpf == userId) {
+      courrentClient = client;
+    }
+  });
+
+  var novoElemento = document.createElement("div");
+  novoElemento.className = "profile-card";
+  novoElemento.innerHTML = `<h1>Cuidador</h1><br/><h2>Nome: ${courrentClient.name} </h2><h2>Experiência: ${courrentClient.monthsExperience} </h2><h2>CPF:  ${courrentClient.cpf}</h2><h2>Telefone: ${courrentClient.phone} </h2><h2>Email:  ${courrentClient.email}</h2>`;
+
+  var main = document.querySelector(".main-content");
+  main.innerHTML = "";
+  main.appendChild(novoElemento);
+}
+
+function openAgenda() {
+  var novoElemento = document.createElement("div");
+  novoElemento.className = "profile-card";
+  novoElemento.innerHTML =
+    "<h1>Cuidador</h1><br/><h2>Nome: RONALDO CUIDADOR</h2><h2>Experiência: ESTAGIÁRIO</h2><h2>CPF: </h2><h2>Telefone: </h2><h2>Email: </h2><h2>Endereço: </h2><h2>CEP: </h2>";
+  var main = document.querySelector(".main-content");
+  main.innerHTML = "";
+  main.appendChild(novoElemento);
+}
+
+//ADD PATIENTS
+
+const addPatientButton = document.getElementById("addPatientButton");
+const addPatientPopup = document.getElementById("addPatientPopup");
+const closeButton = document.querySelector(".closeButton");
+closeButton.addEventListener("click", () => {
+  addPatientPopup.style.display = "none";
+});
+const confirmButton = document.getElementById("confirmButton");
+
+addPatientButton.addEventListener("click", () => {
+  addPatientPopup.style.display = "block";
+});
+
+/**
+ * Event listener para o botão de confirmação do popup de adicionar paciente
+ * Captura os dados no popup e adiciona o paciente caso ele nao exista
+ * Apos enviar os dados para o metodo addPatient, o popup é fechado
+ */
+confirmButton.addEventListener("click", () => {
+  const cpf = document.getElementById("cpf").value;
+  const name = document.getElementById("name").value;
+  const age = document.getElementById("age").value;
+
+  addPatient(cpf, name, age);
+
+  addPatientPopup.style.display = "none";
+});
+
+async function addPatient(cpf, name, age) {
+  const newPatient = {
+    cpf: cpf,
+    name: name,
+    age: age,
+  };
+
+  try {
+    const alreadyRegistered = await fetch(
+      "http://localhost:3000/patients?cpf=${cpf}"
+    );
+    const patientsJson = await alreadyRegistered.json();
+
+    if (patientsJson.length > 0) {
+      alert("Um paciente com o mesmo CPF já está cadastrado!");
+      return; // Retorna sem adicionar o paciente
+    }
+    const response = await fetch("http://localhost:3000/patients", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPatient),
+    });
+
+    if (response.ok) {
+      // Se o paciente foi adicionado com sucesso
+      console.log("Paciente adicionado com sucesso!");
+    } else {
+      // Se ocorreu algum erro ao adicionar o paciente
+      console.log("Erro ao adicionar paciente");
+    }
+  } catch (error) {
+    console.log("Erro ao adicionar paciente:", error);
+  }
+}
+
+//DELETE PATIENTS
+async function deletePatient(cpfToDelete) {
+  try {
+    // Verificar se o paciente existe antes de excluir
+    const response = await fetch(
+      `http://localhost:3000/patients?cpf=${cpfToDelete}`
+    );
+    const patientsJson = await response.json();
+    console.log("Paciente encontrado:", patientsJson);
+
+    if (patientsJson.length === 0) {
+      console.log("Paciente não encontrado!");
+      return; // Retorna sem excluir o paciente
+    }
+
+    // Excluir o paciente caso seja encontrado
+    const patientId = patientsJson[0].id; // Supondo que a API retorna um campo "id" para cada paciente
+    const deleteResponse = await fetch(
+      `http://localhost:3000/patients/${patientId}`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    if (deleteResponse.ok) {
+      loadPatients(); // Recarrega a lista de pacientes após a exclusão
+      console.log("Paciente excluído com sucesso!");
+    } else {
+      console.log("Erro ao excluir paciente");
+    }
+  } catch (error) {
+    console.log("Erro ao excluir paciente:", error);
+  }
+}
+
+/**
+ * funtion to logout
+ */
+function logout() {
+  window.location.href = "index.html";
+}
---
+++ js/script.js
@@ -0,0 +1,131 @@
+/**
+ * HERE WE HAVE THE FUNCTIONS THAT WILL BE USED IN THE LOGIN AND SIGNUP FORMS
+ */
+
+/*
+LOGIN
+*/
+
+function login() {
+  const emailForm = document.getElementById("login-email").value;
+  const passwordForm = document.getElementById("login-password").value;
+  sendLogin({
+    email: emailForm,
+    passwordForm: passwordForm,
+  });
+
+  async function sendLogin(user) {
+    let url = ""; //end point
+
+    try {
+      let response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json;charset=utf-8",
+        },
+      });
+
+      let json = await response.json();
+
+      if (response.status == 200) {
+        localStorage.setItem("token", json.token);
+        localStorage.setItem("userID", json.userID);
+        localStorage.setItem("userName", json.userName);
+        window.location = "dashboard.html";
+      } else {
+        localStorage.removeItem("token");
+        localStorage.removeItem("userID");
+        localStorage.removeItem("userName");
+        alert(json.message);
+      }
+    } catch (e) {
+      window.location = "dashboard.html"; //just for testing!
+      console.log(e);
+    }
+  }
+}
+// triggers login function DO NOT DELETE!
+document.getElementById("commit").addEventListener("click", login, false);
+
+/*  SIGNUP
+/**
+ * @param {Object} user - user object to be created in the database (firstName, lastName, email, password)
+ */
+function createUser() {
+  if (
+    document.getElementById("signup-password").value !=
+    document.getElementById("signup-password-confirmation").value
+  )
+    alert("The passwords do not match!");
+  else
+    create({
+      firstName: document.getElementById("name").value,
+      lastName: document.getElementById("lastname").value,
+      email: document.getElementById("email").value,
+      password: document.getElementById("signup-password").value,
+    });
+}
+async function create(user) {
+  let url = ""; //end point
+
+  try {
+    let response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json; charset=utf-8",
+      },
+    });
+
+    let data = await response.json();
+
+    if (response.status == 201) {
+      alert("Usuário criado com sucesso.");
+      window.location = "index.html";
+    } else {
+      alert(data.message);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+document
+  .getElementById("createUserButton")
+  .addEventListener("click", createUser, false);
+
+/**
+ * TOGGLE BETWEEN LOGIN AND SIGNUP FORMS
+ */
+function showLogin() {
+  let loginButton = document.getElementById("login-icon");
+  loginButton.className = "";
+  loginButton.classList.add("active");
+
+  let signupButton = document.getElementById("signup-icon");
+  signupButton.className = "";
+  signupButton.classList.add("inactive");
+  signupButton.classList.add("underlineHover");
+
+  //toggle forms
+  document.getElementById("login-form").style.display = "block";
+  document.getElementById("signup-form").style.display = "none";
+}
+
+function showSignup() {
+  let signupButton = document.getElementById("signup-icon");
+  signupButton.className = "";
+  signupButton.classList.add("active");
+
+  let loginButton = document.getElementById("login-icon");
+  loginButton.className = "";
+  loginButton.classList.add("inactive"); // underlineHover
+  loginButton.classList.add("underlineHover");
+
+  //toggle forms
+  document.getElementById("login-form").style.display = "none";
+  document.getElementById("signup-form").style.display = "block";
+}
